2e2dfdce10b8ea1f7602f0a491975e032d0f77ea
Patryk Lisik <patrykkbkak@gmail.com>
2019-05-29 06:44:28 +0200

    generating list form data

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,47 +7,13 @@
     class="primary"
   >
     <v-list dense>
-      <v-list-tile>
-        <v-list-tile-content>
-          <v-list-tile-content class="white--text">Dashboard</v-list-tile-content>
-        </v-list-tile-content>
-      </v-list-tile>
-      <v-list-tile>
+      <v-list-tile v-for="link in this.list['User']" :key="link.text" router :to="link.route">
         <v-list-tile-action>
-          <v-icon>date_range</v-icon>
+          <v-icon class="white--text">{{ link.icon }}</v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
-          <v-list-tile-title>
-            Reservations
-          </v-list-tile-title>
+          <v-list-tile-content class="white--text">{{link.text}}</v-list-tile-content>
         </v-list-tile-content>
-      </v-list-tile>
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon>attach_money</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            Invoices
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon>chat</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            Send feedback
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon color="grey darken-1">settings</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>Manage Account</v-list-tile-title>
       </v-list-tile>
     </v-list>
   </v-navigation-drawer>
@@ -56,23 +22,20 @@
 <script>
 export default {
   name: 'SideBar',
-  data: () => ({
-    drawer: true,
-    items: [
-      { icon: 'trending_up', text: 'Most Popular' },
-      { icon: 'subscriptions', text: 'Subscriptions' },
-      { icon: 'history', text: 'History' },
-      { icon: 'featured_play_list', text: 'Playlists' },
-      { icon: 'watch_later', text: 'Watch Later' }
-    ],
-    items2: [
-      { picture: 28, text: 'Joseph' },
-      { picture: 38, text: 'Apple' },
-      { picture: 48, text: 'Xbox Ahoy' },
-      { picture: 58, text: 'Nokia' },
-      { picture: 78, text: 'MKBHD' }
-    ]
-  }),
+  data: () => {
+    return {
+      list: {
+        User: [
+          {icon: 'dashboard', text: 'Dashboard', route: '/'},
+          {icon: 'chat', text: 'Send feedback', route: '/feedback'},
+          {icon: 'attach_money', text: 'Invoices', route: '/invoices'},
+          {icon: 'settings', text: 'Manage Account', route: '/settings'}
+        ],
+        Admin: [],
+        Manager: []
+      }
+    }
+  },
   props: {
     source: String
   }
